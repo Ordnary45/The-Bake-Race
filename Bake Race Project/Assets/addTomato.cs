2c0b07138b51c66899dd642ab0a0e400620261da
cheeseking-m4a <109551749+cheeseking-m4a@users.noreply.github.com>
2026-04-02 16:48:13 -0600

    programmed tomato
    programmed tomato to snap in place. also put a freeze on position/rotation XYZ for the cheese & tomato on the counter.
    
    if this causes issues im fine with reversing that change. just did it for testing purposes without headset

---
+++ Bake Race Project/Assets/addTomato.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class addTomato : MonoBehaviour
+{
+    public GameObject finalTomato;
+    public Transform plate;
+    public float revealDistance = 1.0f;
+
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = finalTomato.GetComponent<Renderer>();
+        rend.enabled = false;
+    }
+
+    void Update()
+    {
+        if (rend.enabled) return;
+
+        GameObject[] buns = GameObject.FindGameObjectsWithTag("Tomato");
+
+        foreach (GameObject bun in buns)
+        {
+            float distance = Vector3.Distance(bun.transform.position, plate.position);
+
+            if (distance <= revealDistance)
+            {
+                rend.enabled = true;
+                bun.SetActive(false);
+                return;
+            }
+        }
+    }
+}
