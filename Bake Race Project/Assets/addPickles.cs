d4355e185b19b1be6b673602e2f715d6c93939c2
cheeseking-m4a <109551749+cheeseking-m4a@users.noreply.github.com>
2026-04-05 16:17:33 -0600

    Tagged & finished pickles
    Pickles should now behave like any other ingredient

--- Bake Race Project/Assets/addPickles.cs
+++ Bake Race Project/Assets/addPickles.cs
@@ -1,39 +1,3 @@
-// using UnityEngine;
-
-// public class addPickles : MonoBehaviour
-// {
-//     public GameObject finalPickles;
-//     public Transform plate;
-//     public float revealDistance = 0.25f; //1.0f;
-
-//     private Renderer rend;
-
-//     void Start()
-//     {
-//         rend = finalPickles.GetComponent<Renderer>();
-//         rend.enabled = false;
-//     }
-
-//     void Update()
-//     {
-//         if (rend.enabled) return;
-
-//         GameObject[] buns = GameObject.FindGameObjectsWithTag("Pickles");
-
-//         foreach (GameObject bun in buns)
-//         {
-//             float distance = Vector3.Distance(bun.transform.position, plate.position);
-
-//             if (distance <= revealDistance)
-//             {
-//                 rend.enabled = true;
-//                 bun.SetActive(false);
-//                 return;
-//             }
-//         }
-//     }
-// }
-
 using UnityEngine;
 
 public class addPickles : MonoBehaviour
